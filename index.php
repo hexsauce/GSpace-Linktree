fdb8c40a255a22e60926bdcbff3aca7c0f9eed5c
Sauce <sonderistic95@gmail.com>
2025-03-14 20:49:59 -0500

    Initial UI created

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GSpace Links </title>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="icon" type="image/x-icon" href="./assets/logo-transparent.png">
+</head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7XNXM6JTFQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-7XNXM6JTFQ');
+</script>
+
+<body>
+  <div class="container">
+    <img src="./assets/logo.jpg" alt="logo" class="logo">
+
+    <div class="links">
+      <a href="#" class="link">✨ Services</a>
+      <a href="#" class="link">📦 Products</a>
+      <a href="https://www.facebook.com/gloriabeautytrainingacademy" class="link" target="_blank">🎓 Academy</a>
+      <a href="https://www.facebook.com/gloriafacial" class="link" target="_blank">🌐 Facebook</a>
+      <a href="https://www.google.com/maps/place/Gloria+Perfect+Aesthetics+Sdn+Bhd/@5.9379447,116.0991648,17z/data=!3m1!4b1!4m6!3m5!1s0x323b68ee0d1019db:0x2f4a5f858e821f5b!8m2!3d5.9379394!4d116.1017397!16s%2Fg%2F1hc1m4vp7?entry=ttu&g_ep=EgoyMDI1MDMxMS4wIKXMDSoASAFQAw%3D%3D" class="link" target="_blank">🗺️ HQ Location</a>
+    </div>
+  </div>
+</body>
+
+</html>
